888c9322aaf2daf0d607aa5a4faf2d507ff59349
Andy <andyeveningstar@gmail.com>
2024-03-21 22:44:12 -0300

    Add feature to: - Take input file - Copy and paste it with new randomized file name and visitor name

--- visitor_generator.php
+++ visitor_generator.php
@@ -2,8 +2,37 @@
 
 # http://localhost/join_avenue_visitor_generator/visitor_generator.php
 
+# Create a random, 6-character name for the visitor and its corresponding file
+function assignRandomFileName() { 
+    $characterSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    $randomName = "";
+    for ($i = 0; $i < 6; $i++) {
+        # Generate a random character from the set
+        $newLetter = $characterSet[rand(0, strlen($characterSet) - 1)];
+        $randomName .= $newLetter;
+    }
+    return $randomName;
+}
+
+function assignVisitorName($randomString) {
+    # Encode string and append terminator
+    $terminator = pack("v", 0xFFFF);
+    $twoByteName = mb_convert_encoding($randomString, "UTF-16LE");
+    $twoByteName .= $terminator;
+    return $twoByteName;
+}
+
+# Create random name for the visitor and corresponding output file
+$newFileName = assignRandomFileName();
+
+function writeVisitorNameToFile($file) {
+    global $newFileName;
+    $visitorName = assignVisitorName($newFileName);
+    fwrite($file, $visitorName);
+}
+
 $inputPath = "C:/xampp/htdocs/join_avenue_visitor_generator/Base Visitors/VISITOR _ Jane.pjv";
-$outputPath = "C:/xampp/htdocs/join_avenue_visitor_generator/Output Visitors/Jane.pjv";
+$outputPath = "C:/xampp/htdocs/join_avenue_visitor_generator/Output Visitors/" . $newFileName . ".pjv";
 
 # Open visitor file, copy it, close original
 $inputVisitorFile = fopen($inputPath, "r+b");
@@ -14,28 +43,7 @@ fclose($inputVisitorFile);
 $outputVisitorFile = fopen($outputPath, "r+b");
 $visitorSize = filesize($outputPath);
 
-# Creates a random, 6-character name + 1 terminator. 16-bit, little endian
-function randomName() {
-    $characterSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    $randomName = "";
-    $terminator = pack("v", 0xFFFF);
-
-    for ($i = 0; $i < 6; $i++) {
-        # Generate a random character from the set and obtain its ASCII number
-        $newLetter = $characterSet[rand(0, strlen($characterSet) - 1)];
-        $newLetter = mb_convert_encoding($newLetter, "UTF-16LE");
-        $randomName .= $newLetter;
-    }
-    $randomName .= $terminator;
-    return $randomName;
-}
-
-function writeName($file) {
-    $name = randomName();
-    fwrite($file, $name);
-}
-
-writeName($outputVisitorFile);
+writeVisitorNameToFile($outputVisitorFile);
 
 fclose($outputVisitorFile);
 
