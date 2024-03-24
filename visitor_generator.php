d76c831c069e81b943b0f0738065a9414bb99dfc
Andy <andyeveningstar@gmail.com>
2024-03-24 18:37:01 -0300

    Implement FakerPHP to generate file and visitor names

--- visitor_generator.php
+++ visitor_generator.php
@@ -2,33 +2,9 @@
 
 # http://localhost/join_avenue_visitor_generator/visitor_generator.php
 
+require_once "C:/xampp/htdocs/join_avenue_visitor_generator/libraries/vendor/autoload.php";
+$faker = Faker\Factory::create();
 $sourceDirectory = "C:/xampp/htdocs/join_avenue_visitor_generator/Base Visitors/";
-
-# Create a random, 6-character name for the visitor and its corresponding file
-function assignRandomFileName() {
-    $characterSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    $randomName = "";
-    for ($i = 0; $i < 6; $i++) {
-        # Generate a random character from the set
-        $newLetter = $characterSet[rand(0, strlen($characterSet) - 1)];
-        $randomName .= $newLetter;
-    }
-    return $randomName;
-}
-
-function assignVisitorName($randomString) {
-    # Encode string and append terminator
-    $terminator = pack("v", 0xFFFF);
-    $twoByteName = mb_convert_encoding($randomString, "UTF-16LE");
-    $twoByteName .= $terminator;
-    return $twoByteName;
-}
-
-function writeVisitorNameToFile($file) {
-    global $newFileName;
-    $visitorName = assignVisitorName($newFileName);
-    fwrite($file, $visitorName);
-}
 
 # Select a random file from the source directory
 function chooseRandomFile($directory) {
@@ -37,18 +13,46 @@ function chooseRandomFile($directory) {
     $directoryArray = array_diff($directoryArray, [".", "..", "desktop.ini"]);
     # Randomize the order or array elements 
     shuffle($directoryArray);
-
     return $directoryArray[0];
 }
 
+# Generate a 6-character name for the visitor and its corresponding file
+function generateFileName() {
+    global $faker;
+    $fileName = ""; 
+    do {
+        $fileName = $faker->firstName;
+    } while (strlen($fileName) != 6);
+
+    return $fileName;
+}
+
+# Encode visitor name string created with `generateFileName()`
+# Append encoded terminator
+function assignVisitorName($oneByteVisitorName) {
+    $terminator = pack("v", 0xFFFF);
+    $twoByteName = mb_convert_encoding($oneByteVisitorName, "UTF-16LE");
+    $twoByteName .= $terminator;
+    return $twoByteName;
+}
+
+# Inject encoded visitor name to file
+function writeVisitorNameToFile($file) {
+    global $newFileName;
+    $visitorName = assignVisitorName($newFileName);
+    fwrite($file, $visitorName);
+}
+
 for ($x = 1; $x <= 8; $x++) {
-    # Create random name for the visitor and corresponding output file
-    $newFileName = assignRandomFileName();
+    # Generate a 6-character name for the visitor and its corresponding file
+    $newFileName = generateFileName();
     
+    # Choose a random file from source directory
     $inputPath = $sourceDirectory . chooseRandomFile($sourceDirectory);
+    # Path to new file
     $outputPath = "C:/xampp/htdocs/join_avenue_visitor_generator/Output Visitors/" . $newFileName . ".pjv";
 
-    # Open visitor file, copy it, close original
+    # Open source file, copy it, close original
     $inputVisitorFile = fopen($inputPath, "r+b");
     $copyFile = copy($inputPath, $outputPath);
     fclose($inputVisitorFile);
