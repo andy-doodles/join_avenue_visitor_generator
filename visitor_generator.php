df96b1c0e05e28ba7953c74960a24e7fe953d6b7
Andy <andyeveningstar@gmail.com>
2024-03-24 18:40:12 -0300

    Change write-to-file mode to prevent entire file from being overwritten

--- visitor_generator.php
+++ visitor_generator.php
@@ -58,7 +58,7 @@ for ($x = 1; $x <= 8; $x++) {
     fclose($inputVisitorFile);
 
     # Modify visitor's name
-    $outputVisitorFile = fopen($outputPath, "w+b");
+    $outputVisitorFile = fopen($outputPath, "r+b");
     writeVisitorNameToFile($outputVisitorFile);
     fclose($outputVisitorFile);
 }
