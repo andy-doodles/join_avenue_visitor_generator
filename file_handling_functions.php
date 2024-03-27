a2b5044b7f97eed1ef6207e2605aab16d476b6b7
Andy <andyeveningstar@gmail.com>
2024-03-27 15:22:51 -0300

    Create separate files for functions

---
+++ file_handling_functions.php
@@ -0,0 +1,69 @@
+<?php
+
+# Select a random file from the source directory
+function chooseRandomFile($directory) {
+    $directoryArray = scandir($directory);
+    # Exclude unwanted directory elements and files
+    $directoryArray = array_diff($directoryArray, [".", "..", "desktop.ini"]);
+    # Randomize the order of array elements
+    shuffle($directoryArray);
+    return $directoryArray[0];
+}
+
+/* Generate a 6-character name for the visitor and its corresponding file
+Gender of name depends on gender created with `generateVisitorGender()` */
+function generateFileName($visitorGender) {
+    global $faker;
+    $fileName = "";
+
+    if ($visitorGender == "man or boy") {
+        do {
+        $fileName = $faker->firstNameMale;
+        } while (strlen($fileName) != 6);
+    }
+    else {
+        do {
+            $fileName = $faker->firstNameFemale;
+        } while (strlen($fileName) != 6);
+    }
+
+    return $fileName;
+}
+
+# Inject encoded visitor name to file
+function writeVisitorNameToFile($file) {
+    global $newFileName;
+    $visitorName = assignVisitorName($newFileName);
+    fwrite($file, $visitorName);
+}
+
+# Inject encoded visitor name to file
+function writeVisitorGenderToFile($file, $gender) {
+    $visitorGender = assignVisitorGender($gender);
+    fseek($file, 0x22);
+    fwrite($file, $visitorGender);
+    rewind($file);
+}
+
+# Assign a sprite that corresponds with the name's gender
+function writeSpriteToFile($file, $gender) {
+    global $maleSprites;
+    global $femaleSprites;
+
+    if ($gender == "man or boy") {
+        $maleSprite = chooseSprite($maleSprites);
+        $hexValue = pack("v", $maleSprite[1]);
+        fseek($file, 0x2A);
+        fwrite($file, $hexValue);
+        rewind($file);
+    }
+    else {
+        $femaleSprite = chooseSprite($femaleSprites);
+        $hexValue = pack("v", $femaleSprite[1]);
+        fseek($file, 0x2A);
+        fwrite($file, $hexValue);
+        rewind($file);
+    }
+}
+
+?>
---
+++ visitor_data_functions.php
@@ -0,0 +1,54 @@
+<?php
+
+function generateVisitorGender() {
+    $genderArray = ["man", "woman"];
+    shuffle($genderArray);
+
+    if ($genderArray[0] == "man") {
+        $visitorGender = "man or boy";
+        return $visitorGender;
+    }
+    else {
+        $visitorGender = "woman or girl";
+        return $visitorGender;
+    }
+}
+
+# Encode visitor gender created with `generateVisitorGender()`
+function assignVisitorGender($gender) {
+    if ($gender == "man or boy") {
+        $visitorGender = pack("v", 0x00);
+        return $visitorGender;
+    }
+    else {
+        $visitorGender = pack("v", 0x10);
+        return $visitorGender;
+    }
+}
+
+/* Encode visitor name string created with `generateFileName()`
+Append encoded terminator */
+function assignVisitorName($oneByteVisitorName) {
+    $terminator = pack("v", 0xFFFF);
+    $twoByteName = mb_convert_encoding($oneByteVisitorName, "UTF-16LE");
+    $twoByteName .= $terminator;
+    return $twoByteName;
+}
+
+# Choose a random sprite from a pool
+function chooseSprite($spriteArray) {
+    $keys = array_keys($spriteArray);
+    $randomKey = $keys[array_rand($keys)];
+    $sprite = $spriteArray[$randomKey];
+    return $sprite;
+}
+
+function getVisitorData($name, $gender) {
+    $visitorArray = [
+        "name" => $name,
+        "gender" => $gender
+    ];
+    return $visitorArray;
+}
+
+?>
