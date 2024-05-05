e5809b3d043e468804fffcd4c15d666953e462ab
Andy <andyeveningstar@gmail.com>
2024-05-05 13:11:43 -0300

    Initialize Pest PHP

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '178fcecddab079386575f0d1cbd685a35ed02431',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '178fcecddab079386575f0d1cbd685a35ed02431',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
