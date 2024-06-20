0683691e81ebd85907a3495e1a088b7af45afccd
Andy <andyeveningstar@gmail.com>
2024-06-20 19:10:04 -0300

    Delete unecessary function writeNumberOfLinkTradesToFile()

--- file_handling_functions.php
+++ file_handling_functions.php
@@ -135,19 +135,6 @@ function writeNumberOfMedalsToFile(
     writeDataToFile($file, 0x39, $visitorMedals);
 }
 
-/* Writes the number of Pokémon link trades the visitor has participated in
-The integer is converted to a 32-bit, little endian signed integer
-Number of link trades is at offset 0x40
-*/
-function writeNumberOfLinkTradesToFile(
-    $file,
-    int $visitorLinkTrades
-)
-{
-    $signedVisitorLinkTrades = pack("l", $visitorLinkTrades);
-    writeDataToFile($file, 0x40, $signedVisitorLinkTrades);
-}
-
 /* Writes to fila any number that needs to be a signed integer in the binary file
 The provided integer is converted to a 32-bit, little endian signed integer
 */
